ef5f43baedd74e64c29b84b89cd541201a2e3829
Lukyanski Artsiom <lansky1994@gmail.com>
2019-08-11 15:46:25 +0300

    add zadanie 3

--- Задание 3/script.js
+++ Задание 3/script.js
@@ -1,46 +1,3 @@
-/*
-let num = 93;
-switch (true) {
-    case num <= 49:
-        console.log("Неверно!");
-        break;
-    case num >= 100:
-        console.log("Много!");
-        break;
-    case num > 80:
-        console.log("Все еще много!");
-        break; 
-    case num == 50:
-        console.log("Вthyj");
-        break; 
-    default:
-        console.log("Чтото не так!")
-        break;
-}
-(num == 50) ? console.log('Верно') : console.log('Неверно');
-
-console.log(console);
-*/
-/*
-let num = 49;
-while (num < 55) {
-    console.log(num);
-    num++;
-}
-
-do {
-    console.log(num);
-    num++;
-}while (num < 50);
-
-for( let i = 1; i < 8; i++ ){
-    if (i == 6){
-       // break;
-       continue;
-    }
-    console.log(i)
-}
-*/
 'use strict';
 
 
@@ -67,7 +24,6 @@ let appData = {
     savings: true
 };
 
-
 function chooseExpenses(){
     for (let i = 0; i < 2; i++) {
         let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
@@ -89,59 +45,13 @@ function chooseExpenses(){
 chooseExpenses();
 
 
-
-
-// Используем цикл WHILE
-
-// let i = 0;
-// while (i < 2) {
-//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt ("Во сколько обойдется?", "");
-
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-//         console.log ("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-
-
-
-// Используем цикл DO...WHILE
-
-// let i = 0;
-// do {
-//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt ("Во сколько обойдется?", "");
-
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-//         console.log ("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-// while(i < 2);
-
-
-
 function detectDayBudget(){
     appData.moneyPerDay = (appData.budget / 30).toFixed(1);
     alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
 }
 
 detectDayBudget();
+
 
 function detectLevel(){
     if (appData.moneyPerDay < 100) {
@@ -156,7 +66,6 @@ function detectLevel(){
 }
 
 detectLevel();
-
 
 
 function chooseOptExpenses(){
@@ -178,6 +87,7 @@ function chooseOptExpenses(){
 
 chooseOptExpenses();
 
+
 function checkSavings(){
     if (appData.savings == true){
         let save = +prompt('Сколько денег на депозите', ''),
@@ -187,4 +97,5 @@ function checkSavings(){
     }
     
 }
+
 checkSavings();
